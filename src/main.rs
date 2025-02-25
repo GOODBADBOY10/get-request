68c9908c98fed06f01964d226eedd2e85181e2d0
GOODBADBOY10 <selfmanademola@gmail.com>
2025-02-25 10:24:28 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use error_chain::error_chain;
 use reqwest;
 use tokio;
 
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() -> Result<()> {
     let res = reqwest::get("http://httpbin.org/get").await?;
 
     println!("Status: {}", res.status());
+    println!("Headers\n: {:#?}", res.headers());
     println!("Body: {}", res.text().await?);
 
     Ok(())
